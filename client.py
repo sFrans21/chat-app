3a1d0f2a047cf929e05525add745d197d72132de
sFrans21 <118550565+sFrans21@users.noreply.github.com>
2024-04-10 21:09:40 +0700

    p

---
+++ client.py
@@ -0,0 +1,23 @@
+import socket
+
+ip = "localhost"
+
+port = 139
+
+sc = socket.socket()
+
+try: 
+      sc.connect((ip,port))
+      print("Terhubung dengan server")
+except:
+      print("Sambungan gagal: (")
+      
+while 1:
+      inbox = sc.recv(1024)
+      inbox = inbox.decode()
+      print("server: ",inbox)
+      
+      pesan = input(str(">>Anda: "))
+      pesan = pesan.encode()
+      sc.send(pesan)
+      
