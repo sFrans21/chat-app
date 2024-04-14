0714dd230b6a13aff1e5830afa6cd18844ceb6c6
sFrans21 <118550565+sFrans21@users.noreply.github.com>
2024-04-14 20:40:49 +0700

    p

--- client.py
+++ client.py
@@ -21,20 +21,3 @@
 #       pesan = pesan.encode()
 #       sc.send(pesan)
       
-      
-from socket import *
-import random
-import time
-import string
-
-serverName = "192.168.56.1"
-serverPort = 12345
-clientSocket = socket(AF_INET, SOCK_DGRAM)
-message = input("Masukkan nama anda: ")
-clientSocket.sendto(message.encode(), (serverName, serverPort))
-
-while True:
-      pesan, serverAddress = clientSocket.recvfrom(2048)
-      message = "Permintaan acak"
-      clientSocket.sendto(message.encode(), (serverName, serverPort))
-      print("Bilangan acak dari server:(" + str(serverAddress)+ "): " + str(pesan.decode()))
--- server.py
+++ server.py
@@ -19,24 +19,3 @@
 #       inbox = conn.recv(1024)
 #       inbox = inbox.decode
 #       print("Client:", inbox)
-
-
-from socket import *
-import time
-import random
-
-serverPort = 12345
-serverIP = "192.168.56.1"
-serverSocket = socket(AF_INET, SOCK_DGRAM)
-serverSocket.bind(serverIP, serverPort)
-print("SERVER BILANGAN ACAK")
-
-while True:
-      message, clientAddress = serverSocket.recvfrom(2048)
-      print(str(message.decode())) + " dari: " + str(clientAddress)
-      
-      bilAcak = random.randint(6,20)\
-            
-      time.sleep(1)
-      
-      serverSocket.sendto(str(bilAcak).encode(), clientAddress)
