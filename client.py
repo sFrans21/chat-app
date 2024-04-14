96ce86cc32d6a56ebacaf8a7bd3b9339c7dcf999
sFrans21 <118550565+sFrans21@users.noreply.github.com>
2024-04-14 20:22:32 +0700

    change

--- client.py
+++ client.py
@@ -1,23 +1,40 @@
-import socket
+# import socket
 
-ip = "localhost"
+# ip = "localhost"
 
-port = 135
+# port = 5505
 
-sc = socket.socket()
+# sc = socket.socket()
 
-try: 
-      sc.connect((ip,port))
-      print("Terhubung dengan server")
-except:
-      print("Sambungan gagal: (")
+# try: 
+#       sc.connect((ip,port))
+#       print("Terhubung dengan server")
+# except:
+#       print("Sambungan gagal: (")
       
-while 1:
-      inbox = sc.recv(1024)
-      inbox = inbox.decode()
-      print("server: ",inbox)
+# while 1:
+#       inbox = sc.recv(1024)
+#       inbox = inbox.decode()
+#       print("server: ",inbox)
       
-      pesan = input(str(">>Anda: "))
-      pesan = pesan.encode()
-      sc.send(pesan)
+#       pesan = input(str(">>Anda: "))
+#       pesan = pesan.encode()
+#       sc.send(pesan)
       
+      
+from socket import *
+import random
+import time
+import string
+
+serverName = "192.168.56.1"
+serverPort = 12345
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+message = input("Masukkan nama anda: ")
+clientSocket.sendto(message.encode(), (serverName, serverPort))
+
+while True:
+      pesan, serverAddress = clientSocket.recvfrom(2048)
+      message = "Permintaan acak"
+      clientSocket.sendto(message.encode(), (serverName, serverPort))
+      print("Bilangan acak dari server:(" + str(serverAddress)+ "): " + str(pesan.decode()))
--- server.py
+++ server.py
@@ -1,21 +1,42 @@
-import socket
+# import socket
 
-ip = socket.gethostname()
-port = 135
+# ip = socket.gethostname()
+# port = 5505
 
-sc = socket.socket()
-sc.bind(ip, port)
-print("Server Diaktifkan")
+# sc = socket.socket()
+# sc.bind(ip, port)
+# print("Server Diaktifkan")
 
-sc.listen(5)
-conn,addr = sc.accept()
-print("Terhubung dengan ", addr)
+# sc.listen(5)
+# conn,addr = sc.accept()
+# print("Terhubung dengan ", addr)
 
-while 1:
-      pesan = input(str(">>Anda: "))
-      pesan = pesan.encode()
-      conn.send(pesan)
+# while 1:
+#       pesan = input(str(">>Anda: "))
+#       pesan = pesan.encode()
+#       conn.send(pesan)
       
-      inbox = conn.recv(1024)
-      inbox = inbox.decode
-      print("Client:", inbox)
+#       inbox = conn.recv(1024)
+#       inbox = inbox.decode
+#       print("Client:", inbox)
+
+
+from socket import *
+import time
+import random
+
+serverPort = 12345
+serverIP = "192.168.56.1"
+serverSocket = socket(AF_INET, SOCK_DGRAM)
+serverSocket.bind(serverIP, serverPort)
+print("SERVER BILANGAN ACAK")
+
+while True:
+      message, clientAddress = serverSocket.recvfrom(2048)
+      print(str(message.decode())) + " dari: " + str(clientAddress)
+      
+      bilAcak = random.randint(6,20)\
+            
+      time.sleep(1)
+      
+      serverSocket.sendto(str(bilAcak).encode(), clientAddress)
